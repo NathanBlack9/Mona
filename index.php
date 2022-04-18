2d869259a66968b9b01206aa25a51bf1bd7334cd
Suren <suren.khachatryan1999@bk.ru>
2022-04-18 19:24:01 +0300

    Начало

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+  <title>Mona</title>
+</head>
+
+<body>
+  <div class="asd"></div>
+</body>
+
+</html>
