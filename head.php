d795f49e23fc08ddcaa6fec613144be4ed1a1f29
Suren <suren.khachatryan1999@bk.ru>
2022-12-18 11:31:35 +0300

    Yandex webmaster: add yandex counter

--- head.php
+++ head.php
@@ -23,5 +23,22 @@
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/build/styles/vendor/jquery-confirm.min.css">
 
   <meta name="yandex-verification" content="8f99225d1e8c9cdc" />
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(91743796, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+    });
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/91743796" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  <!-- /Yandex.Metrika counter -->
   <?php wp_head(); ?>
 </head>
