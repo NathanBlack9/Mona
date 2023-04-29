6afc8c74aa22ea60197f628f51082af71f920007
Suren <suren.khachatryan1999@bk.ru>
2023-04-29 22:24:40 +0300

    cache control - no-cache

--- head.php
+++ head.php
@@ -1,6 +1,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Cache-control" content="no-cache">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/build/img/apple-touch-icon.png">
