2b031534066895005d77ee2fad69473f5e9a2f80
Suren <suren.khachatryan1999@bk.ru>
2024-03-01 11:40:49 +0300

    открыл март

--- backend/subscribe.php
+++ backend/subscribe.php
@@ -7,8 +7,8 @@ if($config['type'] == 'dev') {
   $mysqli = new mysqli("localhost", "cx88992_mona", "gx7wkWp4", "cx88992_mona");
 }
   if(isset($_POST['subscribeEmail'])){
-    $object = json_decode($_POST['subscribeEmail'], true); 
-  
+    $object = json_decode($_POST['subscribeEmail'], true);
+
     $config = require "config.php";
     if($config['type'] == 'dev') {
       $mysqli = new mysqli("localhost", "root", "", "mona");
@@ -17,15 +17,15 @@ if($config['type'] == 'dev') {
     }
 
     $email = $mysqli->real_escape_string($object['email']);
-  
+
     $queryInsert = $mysqli->query("insert INTO alert(email) VALUES ('$email');");
-  
+
     if (!$queryInsert) {
       echo false;
     } else {
       echo true;
     }
-    
+
   } else {
     echo json_encode('Ошибка!!!');
   }
--- src/js/modules/sign.js
+++ src/js/modules/sign.js
@@ -295,7 +295,7 @@ $(() => {
   // инициализировать только при $(document).ready() !!!
   $('#sign-form__date').datepicker({
     minDate: 0,
-    maxDate: '2024-02-29',
+    maxDate: '2024-03-31',
     dateFormat: "yy-mm-dd",
     onSelect: function( date, element ) {
 
