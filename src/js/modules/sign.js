8ea8bef9cee868ff87962ff636f0eb687084a1f6
Suren <suren.khachatryan1999@bk.ru>
2024-03-30 11:03:25 +0300

    открыл апрель

--- src/js/modules/sign.js
+++ src/js/modules/sign.js
@@ -294,7 +294,7 @@ $(() => {
   // инициализировать только при $(document).ready() !!!
   $('#sign-form__date').datepicker({
     minDate: 0,
-    maxDate: '2024-03-31',
+    maxDate: '2024-04-30',
     dateFormat: "yy-mm-dd",
     onSelect: function( date, element ) {
 
